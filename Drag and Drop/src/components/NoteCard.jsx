57284aeb8e2b0bab4aad7013f5ba802803c30107
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-08-23 17:00:29 +0200

    fronend-done

--- Drag and Drop/src/components/NoteCard.jsx
+++ Drag and Drop/src/components/NoteCard.jsx
@@ -1,11 +1,15 @@
 import Trash from "../icons/Trash"
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
+import {setNewOffset, autoGrow, setZIndex} from '../utils'
 
 const NoteCard = ({note}) => {
 
   const body = JSON.parse(note.body)
-  const position = JSON.parse(note.position)
+  const [position, setPosition] = useState(JSON.parse(note.position))
   const colors = JSON.parse(note.colors)
+
+  let mouseStartPos = {x:0, y:0}
+  const cardRef = useRef(null)
 
   const textAreaRef = useRef(null)
 
@@ -13,22 +17,47 @@ const NoteCard = ({note}) => {
     autoGrow(textAreaRef)
   },[])
 
-  const autoGrow = (textarea) =>{
-    const {current} = textAreaRef
+  const mouseDown = (e) =>{
+    mouseStartPos.x = e.clientX
+    mouseStartPos.y = e.clientY
 
-    current.style.height = "auto";
-    current.style.height = current.scrollHeight + "px"
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+
+    setZIndex(cardRef.current)
   }
+
+  const mouseMove = (e) =>{
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY
+    }
+
+
+    mouseStartPos.x = e.clientX
+    mouseStartPos.y = e.clientY
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDir)
+
+    setPosition(newPosition)
+  }
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
+  
  
-  return <div className="card" style={{backgroundColor:colors.colorBody, left:`${position.x}px`, top:`${position.y}px`}}>
+  return <div ref={cardRef} className="card" style={{backgroundColor:colors.colorBody, left:`${position.x}px`, top:`${position.y}px`}}>
     
 
-    <div className="card-header" style={{ backgroundColor: colors.colorHeader}}>
+    <div onMouseDown={mouseDown} className="card-header" style={{ backgroundColor: colors.colorHeader}}>
       <Trash />
     </div>
 
     <div className="card-body">
-      <textarea onInput={()=>{autoGrow(textAreaRef)}} ref={textAreaRef} defaultValue={body} style={{color:colors.colorText}}>
+      <textarea onFocus={()=> setZIndex(cardRef.current)} onInput={()=>{autoGrow(textAreaRef)}} ref={textAreaRef} defaultValue={body} style={{color:colors.colorText}}>
       </textarea>
     </div>
 
